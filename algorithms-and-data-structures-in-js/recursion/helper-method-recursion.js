4ad6a73308ddd6537941876ab7c6391320c373fe
Theodros Yimer <12742021+theodrosyimer@users.noreply.github.com>
2024-04-25 11:57:52 +0200

    feat: add benchmark to implementation

--- algorithms-and-data-structures-in-js/recursion/helper-method-recursion.js
+++ algorithms-and-data-structures-in-js/recursion/helper-method-recursion.js
@@ -1,22 +1,50 @@
+let start = performance.now()
+collectOddValues([1, 2, 3, 4, 5, 6, 7, 8, 9])
+console.log(performance.now() - start)
+
+let start2 =performance.now()
+collectOddValues2([1, 2, 3, 4, 5, 6, 7, 8, 9])
+console.log(performance.now() - start2)
+
 function collectOddValues(arr){
-    
+
     let result = [];
 
     function helper(helperInput){
         if(helperInput.length === 0) {
             return;
         }
-        
+
         if(helperInput[0] % 2 !== 0){
             result.push(helperInput[0])
         }
-        
+
         helper(helperInput.slice(1))
     }
-    
+
     helper(arr)
 
     return result;
 }
 
-collectOddValues([1,2,3,4,5,6,7,8,9])
+function collectOddValues2(arr){
+    let copyArr = [...arr]
+    let result = [];
+
+    function helper(helperInput){
+        if(helperInput.length === 0) {
+            return;
+        }
+
+        if(helperInput[helperInput.length -1] % 2 !== 0){
+            result.push(helperInput[helperInput.length -1])
+        }
+
+        helperInput.pop()
+        helper(helperInput)
+    }
+
+    helper(copyArr)
+
+    return result;
+}
