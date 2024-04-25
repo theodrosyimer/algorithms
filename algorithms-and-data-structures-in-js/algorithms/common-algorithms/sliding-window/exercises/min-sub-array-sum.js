9274c26b395556c7002a7087dc1e18fbe0d830e1
Theodros Yimer <12742021+theodrosyimer@users.noreply.github.com>
2024-04-25 11:58:06 +0200

    style: fix typo

--- algorithms-and-data-structures-in-js/algorithms/common-algorithms/sliding-window/exercises/min-sub-array-sum.js
+++ algorithms-and-data-structures-in-js/algorithms/common-algorithms/sliding-window/exercises/min-sub-array-sum.js
@@ -1,4 +1,4 @@
-5function minSubArrayLen1(nums, sum) {
+function minSubArrayLen1(nums, sum) {
   let total = 0
   let start = 0
   let end = 0
@@ -28,7 +28,7 @@
 }
 
 minSubArrayLen1([1, 4, 16, 22, 5, 7, 8, 9, 10], 95) // ?
-// minSubArrayLen1([2, 1, 6, 5, 4], 9) // ?
+minSubArrayLen1([2, 1, 6, 5, 4], 9) // ?
 // minSubArrayLen1([3, 1, 7, 11, 2, 9, 8, 21, 62, 33, 19], 52) // ?
 // minSubArrayLen1([1, 4, 16, 22, 5, 7, 8, 9, 10], 39) // ?
 minSubArrayLen1([1, 1, 4, 10, 5, 7, 8, 9, 10], 19) // ?
